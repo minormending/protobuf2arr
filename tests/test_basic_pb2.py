4a3c4bccca7b249c004b9cbb5ac48e67499854d4
minormending <54426030+minormending@users.noreply.github.com>
2022-04-11 09:33:58 -0400

    add more tests for the alternate model

--- tests/test_basic_pb2.py
+++ tests/test_basic_pb2.py
@@ -6,7 +6,6 @@ from google.protobuf.internal import builder as _builder
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import symbol_database as _symbol_database
-
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
@@ -15,62 +14,44 @@ _sym_db = _symbol_database.Default()
 from google.protobuf import descriptor_pb2 as google_dot_protobuf_dot_descriptor__pb2
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
-    b'\n\x10test_basic.proto\x12\x0ctest_default\x1a google/protobuf/descriptor.proto"\xc4\x04\n\tTestQueue\x12\x18\n\tfield_int\x18\x01 \x01(\x05\x42\x05\xda\xb6\x18\x01\x30\x12\x1d\n\x0c\x66ield_double\x18\x02 \x01(\x01\x42\x07\xda\xb6\x18\x03\x30.0\x12\x1a\n\x0c\x66ield_string\x18\x03 \x01(\tB\x04\xda\xb6\x18\x00\x12\x1d\n\nfield_bool\x18\x04 \x01(\x08\x42\t\xda\xb6\x18\x05\x46\x61lse\x12\x19\n\x0b\x66ield_bytes\x18\x05 \x01(\x0c\x42\x04\xda\xb6\x18\x00\x12;\n\nfield_enum\x18\x06 \x01(\x0e\x32 .test_default.TestQueue.TestEnumB\x05\xda\xb6\x18\x01\x30\x12\x35\n\x05items\x18\x07 \x03(\x0b\x32 .test_default.TestQueue.TestItemB\x04\xda\xb6\x18\x00\x1a\xf4\x01\n\x08TestItem\x12\x1d\n\x0eitem_field_int\x18\x01 \x01(\x05\x42\x05\xda\xb6\x18\x01\x30\x12"\n\x11item_field_double\x18\x02 \x01(\x01\x42\x07\xda\xb6\x18\x03\x30.0\x12\x1f\n\x11item_field_string\x18\x03 \x01(\tB\x04\xda\xb6\x18\x00\x12"\n\x0fitem_field_bool\x18\x04 \x01(\x08\x42\t\xda\xb6\x18\x05\x46\x61lse\x12\x1e\n\x10item_field_bytes\x18\x05 \x01(\x0c\x42\x04\xda\xb6\x18\x00\x12@\n\x0fitem_field_enum\x18\x06 \x01(\x0e\x32 .test_default.TestQueue.TestEnumB\x05\xda\xb6\x18\x01\x30"=\n\x08TestEnum\x12\x0f\n\x0bTEST_ENUM_0\x10\x00\x12\x0f\n\x0bTEST_ENUM_1\x10\x01\x12\x0f\n\x0bTEST_ENUM_2\x10\x02:4\n\x08nullable\x12\x1d.google.protobuf.FieldOptions\x18\xeb\x86\x03 \x01(\t\x88\x01\x01\x62\x06proto3'
-)
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x10test_basic.proto\x12\x0ctest_default\x1a google/protobuf/descriptor.proto\"\xc4\x04\n\tTestQueue\x12\x18\n\tfield_int\x18\x01 \x01(\x05\x42\x05\xda\xb6\x18\x01\x30\x12\x1d\n\x0c\x66ield_double\x18\x02 \x01(\x01\x42\x07\xda\xb6\x18\x03\x30.0\x12\x1a\n\x0c\x66ield_string\x18\x03 \x01(\tB\x04\xda\xb6\x18\x00\x12\x1d\n\nfield_bool\x18\x04 \x01(\x08\x42\t\xda\xb6\x18\x05\x46\x61lse\x12\x19\n\x0b\x66ield_bytes\x18\x05 \x01(\x0c\x42\x04\xda\xb6\x18\x00\x12;\n\nfield_enum\x18\x06 \x01(\x0e\x32 .test_default.TestQueue.TestEnumB\x05\xda\xb6\x18\x01\x30\x12\x35\n\x05items\x18\x07 \x03(\x0b\x32 .test_default.TestQueue.TestItemB\x04\xda\xb6\x18\x00\x1a\xf4\x01\n\x08TestItem\x12\x1d\n\x0eitem_field_int\x18\x01 \x01(\x05\x42\x05\xda\xb6\x18\x01\x30\x12\"\n\x11item_field_double\x18\x02 \x01(\x01\x42\x07\xda\xb6\x18\x03\x30.0\x12\x1f\n\x11item_field_string\x18\x03 \x01(\tB\x04\xda\xb6\x18\x00\x12\"\n\x0fitem_field_bool\x18\x04 \x01(\x08\x42\t\xda\xb6\x18\x05\x46\x61lse\x12\x1e\n\x10item_field_bytes\x18\x05 \x01(\x0c\x42\x04\xda\xb6\x18\x00\x12@\n\x0fitem_field_enum\x18\x06 \x01(\x0e\x32 .test_default.TestQueue.TestEnumB\x05\xda\xb6\x18\x01\x30\"=\n\x08TestEnum\x12\x0f\n\x0bTEST_ENUM_0\x10\x00\x12\x0f\n\x0bTEST_ENUM_1\x10\x01\x12\x0f\n\x0bTEST_ENUM_2\x10\x02:4\n\x08nullable\x12\x1d.google.protobuf.FieldOptions\x18\xeb\x86\x03 \x01(\t\x88\x01\x01\x62\x06proto3')
 
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, globals())
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, "test_basic_pb2", globals())
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'test_basic_pb2', globals())
 if _descriptor._USE_C_DESCRIPTORS == False:
-    google_dot_protobuf_dot_descriptor__pb2.FieldOptions.RegisterExtension(nullable)
+  google_dot_protobuf_dot_descriptor__pb2.FieldOptions.RegisterExtension(nullable)
 
-    DESCRIPTOR._options = None
-    _TESTQUEUE_TESTITEM.fields_by_name["item_field_int"]._options = None
-    _TESTQUEUE_TESTITEM.fields_by_name[
-        "item_field_int"
-    ]._serialized_options = b"\332\266\030\0010"
-    _TESTQUEUE_TESTITEM.fields_by_name["item_field_double"]._options = None
-    _TESTQUEUE_TESTITEM.fields_by_name[
-        "item_field_double"
-    ]._serialized_options = b"\332\266\030\0030.0"
-    _TESTQUEUE_TESTITEM.fields_by_name["item_field_string"]._options = None
-    _TESTQUEUE_TESTITEM.fields_by_name[
-        "item_field_string"
-    ]._serialized_options = b"\332\266\030\000"
-    _TESTQUEUE_TESTITEM.fields_by_name["item_field_bool"]._options = None
-    _TESTQUEUE_TESTITEM.fields_by_name[
-        "item_field_bool"
-    ]._serialized_options = b"\332\266\030\005False"
-    _TESTQUEUE_TESTITEM.fields_by_name["item_field_bytes"]._options = None
-    _TESTQUEUE_TESTITEM.fields_by_name[
-        "item_field_bytes"
-    ]._serialized_options = b"\332\266\030\000"
-    _TESTQUEUE_TESTITEM.fields_by_name["item_field_enum"]._options = None
-    _TESTQUEUE_TESTITEM.fields_by_name[
-        "item_field_enum"
-    ]._serialized_options = b"\332\266\030\0010"
-    _TESTQUEUE.fields_by_name["field_int"]._options = None
-    _TESTQUEUE.fields_by_name["field_int"]._serialized_options = b"\332\266\030\0010"
-    _TESTQUEUE.fields_by_name["field_double"]._options = None
-    _TESTQUEUE.fields_by_name[
-        "field_double"
-    ]._serialized_options = b"\332\266\030\0030.0"
-    _TESTQUEUE.fields_by_name["field_string"]._options = None
-    _TESTQUEUE.fields_by_name["field_string"]._serialized_options = b"\332\266\030\000"
-    _TESTQUEUE.fields_by_name["field_bool"]._options = None
-    _TESTQUEUE.fields_by_name[
-        "field_bool"
-    ]._serialized_options = b"\332\266\030\005False"
-    _TESTQUEUE.fields_by_name["field_bytes"]._options = None
-    _TESTQUEUE.fields_by_name["field_bytes"]._serialized_options = b"\332\266\030\000"
-    _TESTQUEUE.fields_by_name["field_enum"]._options = None
-    _TESTQUEUE.fields_by_name["field_enum"]._serialized_options = b"\332\266\030\0010"
-    _TESTQUEUE.fields_by_name["items"]._options = None
-    _TESTQUEUE.fields_by_name["items"]._serialized_options = b"\332\266\030\000"
-    _TESTQUEUE._serialized_start = 69
-    _TESTQUEUE._serialized_end = 649
-    _TESTQUEUE_TESTITEM._serialized_start = 342
-    _TESTQUEUE_TESTITEM._serialized_end = 586
-    _TESTQUEUE_TESTENUM._serialized_start = 588
-    _TESTQUEUE_TESTENUM._serialized_end = 649
+  DESCRIPTOR._options = None
+  _TESTQUEUE_TESTITEM.fields_by_name['item_field_int']._options = None
+  _TESTQUEUE_TESTITEM.fields_by_name['item_field_int']._serialized_options = b'\332\266\030\0010'
+  _TESTQUEUE_TESTITEM.fields_by_name['item_field_double']._options = None
+  _TESTQUEUE_TESTITEM.fields_by_name['item_field_double']._serialized_options = b'\332\266\030\0030.0'
+  _TESTQUEUE_TESTITEM.fields_by_name['item_field_string']._options = None
+  _TESTQUEUE_TESTITEM.fields_by_name['item_field_string']._serialized_options = b'\332\266\030\000'
+  _TESTQUEUE_TESTITEM.fields_by_name['item_field_bool']._options = None
+  _TESTQUEUE_TESTITEM.fields_by_name['item_field_bool']._serialized_options = b'\332\266\030\005False'
+  _TESTQUEUE_TESTITEM.fields_by_name['item_field_bytes']._options = None
+  _TESTQUEUE_TESTITEM.fields_by_name['item_field_bytes']._serialized_options = b'\332\266\030\000'
+  _TESTQUEUE_TESTITEM.fields_by_name['item_field_enum']._options = None
+  _TESTQUEUE_TESTITEM.fields_by_name['item_field_enum']._serialized_options = b'\332\266\030\0010'
+  _TESTQUEUE.fields_by_name['field_int']._options = None
+  _TESTQUEUE.fields_by_name['field_int']._serialized_options = b'\332\266\030\0010'
+  _TESTQUEUE.fields_by_name['field_double']._options = None
+  _TESTQUEUE.fields_by_name['field_double']._serialized_options = b'\332\266\030\0030.0'
+  _TESTQUEUE.fields_by_name['field_string']._options = None
+  _TESTQUEUE.fields_by_name['field_string']._serialized_options = b'\332\266\030\000'
+  _TESTQUEUE.fields_by_name['field_bool']._options = None
+  _TESTQUEUE.fields_by_name['field_bool']._serialized_options = b'\332\266\030\005False'
+  _TESTQUEUE.fields_by_name['field_bytes']._options = None
+  _TESTQUEUE.fields_by_name['field_bytes']._serialized_options = b'\332\266\030\000'
+  _TESTQUEUE.fields_by_name['field_enum']._options = None
+  _TESTQUEUE.fields_by_name['field_enum']._serialized_options = b'\332\266\030\0010'
+  _TESTQUEUE.fields_by_name['items']._options = None
+  _TESTQUEUE.fields_by_name['items']._serialized_options = b'\332\266\030\000'
+  _TESTQUEUE._serialized_start=69
+  _TESTQUEUE._serialized_end=649
+  _TESTQUEUE_TESTITEM._serialized_start=342
+  _TESTQUEUE_TESTITEM._serialized_end=586
+  _TESTQUEUE_TESTENUM._serialized_start=588
+  _TESTQUEUE_TESTENUM._serialized_end=649
 # @@protoc_insertion_point(module_scope)
--- tests/test_serializer.py
+++ tests/test_serializer.py
@@ -1,5 +1,6 @@
+from google.protobuf.message import Message
+from typing import Any, List
 from unittest import TestCase
-from google.protobuf.json_format import MessageToDict
 
 from protobuf2arr.serializer import (
     serialize_msg2arr,
@@ -8,9 +9,18 @@ from protobuf2arr.serializer import (
     arr_to_msg,
 )
 from test_basic_pb2 import TestQueue as TestQueueBasic
+from test_alternate_default_pb2 import TestQueueAlt
 
 
 class TestSerializer(TestCase):
+    def _test_deserialization(self, queue: Message, arr: List[Any], serial: str, message_cls: Message) -> Message:
+        msg = arr_to_msg(arr, message_cls())
+        self.assertEqual(queue, msg)
+
+        deserial = deserialize_arr2msg(serial, message_cls())
+        self.assertEqual(queue, deserial)
+        return deserial
+
     def test_serialization_basic_default_empty(self):
         queue = TestQueueBasic()
         arr = msg_to_arr(queue)
@@ -19,13 +29,7 @@ class TestSerializer(TestCase):
         serial = serialize_msg2arr(queue)
         self.assertEqual(serial, "[null,null,null,null,null,null,[]]")
 
-        msg = arr_to_msg(arr, TestQueueBasic())
-        self.assertEqual(MessageToDict(queue), MessageToDict(msg))
-        self.assertEqual(queue, msg)
-
-        deserial = deserialize_arr2msg(serial, TestQueueBasic())
-        self.assertEqual(MessageToDict(queue), MessageToDict(deserial))
-        self.assertEqual(queue, deserial)
+        self._test_deserialization(queue, arr, serial, TestQueueBasic)
 
     def test_serialization_basic_default_empty_subitems(self):
         queue = TestQueueBasic()
@@ -36,13 +40,7 @@ class TestSerializer(TestCase):
         serial = serialize_msg2arr(queue)
         self.assertEqual(serial, "[null,null,null,null,null,null,[null]]")
 
-        msg = arr_to_msg(arr, TestQueueBasic())
-        self.assertEqual(MessageToDict(queue), MessageToDict(msg))
-        self.assertEqual(queue, msg)
-
-        deserial = deserialize_arr2msg(serial, TestQueueBasic())
-        self.assertEqual(MessageToDict(queue), MessageToDict(deserial))
-        self.assertEqual(queue, deserial)
+        self._test_deserialization(queue, arr, serial, TestQueueBasic)
 
     def test_serialization_basic_default_filled(self):
         queue = TestQueueBasic()
@@ -59,13 +57,7 @@ class TestSerializer(TestCase):
         serial = serialize_msg2arr(queue)
         self.assertEqual(serial, "[null,null,null,null,null,null,[]]")
 
-        msg = arr_to_msg(arr, TestQueueBasic())
-        self.assertEqual(MessageToDict(queue), MessageToDict(msg))
-        self.assertEqual(queue, msg)
-
-        deserial = deserialize_arr2msg(serial, TestQueueBasic())
-        self.assertEqual(MessageToDict(queue), MessageToDict(deserial))
-        self.assertEqual(queue, deserial)
+        self._test_deserialization(queue, arr, serial, TestQueueBasic)
 
     def test_serialization_basic(self):
         queue = TestQueueBasic()
@@ -82,13 +74,7 @@ class TestSerializer(TestCase):
         serial = serialize_msg2arr(queue)
         self.assertEqual(serial, '[100,77.89,"Hello World",true,"bytes",1,[]]')
 
-        msg = arr_to_msg(arr, TestQueueBasic())
-        self.assertEqual(MessageToDict(queue), MessageToDict(msg))
-        self.assertEqual(queue, msg)
-
-        deserial = deserialize_arr2msg(serial, TestQueueBasic())
-        self.assertEqual(MessageToDict(queue), MessageToDict(deserial))
-        self.assertEqual(queue, deserial)
+        self._test_deserialization(queue, arr, serial, TestQueueBasic)
 
     def test_serialization_basic_with_subitems(self):
         queue = TestQueueBasic()
@@ -128,10 +114,60 @@ class TestSerializer(TestCase):
             '[100,77.89,"Hello World",true,"bytes",1,[[45,23.67,"Hello Inner World",true,"bytes inner",2]]]',
         )
 
-        msg = arr_to_msg(arr, TestQueueBasic())
-        self.assertEqual(MessageToDict(queue), MessageToDict(msg))
-        self.assertEqual(queue, msg)
+        self._test_deserialization(queue, arr, serial, TestQueueBasic)
 
-        deserial = deserialize_arr2msg(serial, TestQueueBasic())
-        self.assertEqual(MessageToDict(queue), MessageToDict(deserial))
-        self.assertEqual(queue, deserial)
+    def test_serialization_alternate_default_empty(self):
+        queue = TestQueueAlt()
+        arr = msg_to_arr(queue)
+        self.assertEqual(arr, [0, 0.0, '', False, b'', 0, []])
+
+        serial = serialize_msg2arr(queue)
+        self.assertEqual(serial, '[0,0.0,"",false,"",0,[]]')
+
+        self._test_deserialization(queue, arr, serial, TestQueueAlt)
+
+    def test_serialization_alternate_default_empty_subitems(self):
+        queue = TestQueueAlt()
+        queue.items.append(TestQueueAlt.TestItem())
+        arr = msg_to_arr(queue)
+        self.assertEqual(arr, [0, 0.0, '', False, b'', 0, [[0, 0.0, '', False, b'', 0]]])
+
+        serial = serialize_msg2arr(queue)
+        self.assertEqual(serial, '[0,0.0,"",false,"",0,[[0,0.0,"",false,"",0]]]')
+
+        self._test_deserialization(queue, arr, serial, TestQueueAlt)
+    
+    def test_serialization_alternate_default_filled(self):
+        queue = TestQueueAlt()
+        queue.field_int = 1
+        queue.field_double = 1
+        queue.field_string = "null"
+        queue.field_bool = True
+        queue.field_bytes = b"1"
+        queue.field_enum = TestQueueAlt.TestEnum.TEST_ENUM_1
+
+        arr = msg_to_arr(queue)
+        self.assertEqual(arr, [None, None, None, None, None, None, []])
+
+        serial = serialize_msg2arr(queue)
+        self.assertEqual(serial, "[null,null,null,null,null,null,[]]")
+
+        deserial = self._test_deserialization(queue, arr, serial, TestQueueAlt)
+        self.assertEqual(deserial.field_string, "null")
+
+    def test_serialization_alternate(self):
+        queue = TestQueueAlt()
+        queue.field_int = 100
+        queue.field_double = 77.89
+        queue.field_string = "Hello World"
+        queue.field_bool = False
+        queue.field_bytes = b"bytes"
+        queue.field_enum = TestQueueAlt.TestEnum.TEST_ENUM_0
+
+        arr = msg_to_arr(queue)
+        self.assertEqual(arr, [100, 77.89, "Hello World", False, b"bytes", 0, []])
+
+        serial = serialize_msg2arr(queue)
+        self.assertEqual(serial, '[100,77.89,"Hello World",false,"bytes",0,[]]')
+
+        self._test_deserialization(queue, arr, serial, TestQueueAlt)
